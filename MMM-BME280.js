041ab94e6ebe1e652a366dac15f931a35ea38b87
Daniel Habenicht <daniel-habenicht@outlook.de>
2021-04-10 16:49:46 +0200

    send data to iotplotter

--- MMM-BME280.js
+++ MMM-BME280.js
@@ -1,120 +1,117 @@
-
 Module.register("MMM-BME280", {
-    // Default module config.
-    defaults: {
-        updateInterval: 100, // Seconds
-        titleText: "Home weather",
-        deviceAddress: "0x76",
-        temperatureScaleType: 0, // Celsuis
-        pressureScaleType: 0 // hPa
-    },
+  // Default module config.
+  defaults: {
+    updateInterval: 100, // Seconds
+    titleText: "Home weather",
+    deviceAddress: "0x76",
+    temperatureScaleType: 0, // Celsuis
+    pressureScaleType: 0 // hPa
+  },
 
-    // Define start sequence.
-    start: function () {
-        Log.info("Starting module: " + this.name);
+  // Define start sequence.
+  start: function () {
+    Log.info("Starting module: " + this.name);
 
-        this.temperature = 'Loading...';
-        this.humidity = 'Loading...';
-        this.pressure = 'Loading...';
+    this.temperature = "Loading...";
+    this.humidity = "Loading...";
+    this.pressure = "Loading...";
 
-        this.update();
-        setInterval(
-            this.update.bind(this),
-            this.config.updateInterval * 1000);
-    },
+    this.update();
+    setInterval(this.update.bind(this), this.config.updateInterval * 1000);
+  },
 
-    update: function () {
-        this.sendSocketNotification('REQUEST', this.config);
-    },
+  update: function () {
+    this.sendSocketNotification("REQUEST", this.config);
+  },
 
-    getStyles: function () {
-        return ['MMM-BME280.css'];
-    },
+  getStyles: function () {
+    return ["MMM-BME280.css"];
+  },
 
-    // Override dom generator.
-    getDom: function () {
-        var wrapper = document.createElement("div");
+  // Override dom generator.
+  getDom: function () {
+    var wrapper = document.createElement("div");
 
-        var header = document.createElement("div");
-        var label = document.createTextNode(this.config.titleText);
-        header.className = 'bme-header';
-        header.appendChild(label)
-        wrapper.appendChild(header);
+    var header = document.createElement("div");
+    var label = document.createTextNode(this.config.titleText);
+    header.className = "bme-header";
+    header.appendChild(label);
+    wrapper.appendChild(header);
 
-        var table = document.createElement("table");
-        var tbdy = document.createElement('tbody');
-        for (var i = 0; i < 3; i++) {
-            var val = "";
-            var sufix = "";
-            var icon_img = "";
+    var table = document.createElement("table");
+    var tbdy = document.createElement("tbody");
+    for (var i = 0; i < 3; i++) {
+      var val = "";
+      var sufix = "";
+      var icon_img = "";
 
-            switch (i) {
-                case 0:
-                    switch (this.config.temperatureScaleType) {
-                        case 0: // Celsius
-                            val = this.temperature;
-                            sufix = "°C";
-                            break;
-                        case 1: // Fahrenheit
-                            val = Math.round(this.temperature * 9.0 / 5.0 + 32.0);
-                            sufix = "°F";
-                            break;
-                    }
-                    icon_img = "temperature-high";
-                    break;
-                case 1:
-                    val = this.humidity;
-                    icon_img = "tint";
-                    sufix = "%";
-                    break;
-                case 2:
-                    switch (this.config.pressureScaleType) {
-                        case 0: // hPa
-                            val = this.pressure;
-                            sufix = " hPa";
-                            break;
-                        case 1: // inHg
-                            val = Math.round(this.pressure * 100 / 33.864) / 100;
-                            sufix = " inHg";
-                            break;
-                    }
-                    icon_img = "tachometer-alt";
-                    break;
-            }
+      switch (i) {
+        case 0:
+          switch (this.config.temperatureScaleType) {
+            case 0: // Celsius
+              val = this.temperature;
+              sufix = "°C";
+              break;
+            case 1: // Fahrenheit
+              val = Math.round((this.temperature * 9.0) / 5.0 + 32.0);
+              sufix = "°F";
+              break;
+          }
+          icon_img = "temperature-high";
+          break;
+        case 1:
+          val = this.humidity;
+          icon_img = "tint";
+          sufix = "%";
+          break;
+        case 2:
+          switch (this.config.pressureScaleType) {
+            case 0: // hPa
+              val = this.pressure;
+              sufix = " hPa";
+              break;
+            case 1: // inHg
+              val = Math.round((this.pressure * 100) / 33.864) / 100;
+              sufix = " inHg";
+              break;
+          }
+          icon_img = "tachometer-alt";
+          break;
+      }
 
-            var tr = document.createElement('tr');
-            var icon = document.createElement("i");
+      var tr = document.createElement("tr");
+      var icon = document.createElement("i");
 
-            icon.className = 'fa fa-' + icon_img + ' bme-icon';
+      icon.className = "fa fa-" + icon_img + " bme-icon";
 
-            var text_div = document.createElement("div");
-            var text = document.createTextNode(" " + val + sufix);
-            text_div.className = 'bme-text';
-            text_div.appendChild(text);
+      var text_div = document.createElement("div");
+      var text = document.createTextNode(" " + val + sufix);
+      text_div.className = "bme-text";
+      text_div.appendChild(text);
 
-            var td = document.createElement('td');
-            td.className = 'bme-td-icon';
-            td.appendChild(icon)
-            tr.appendChild(td)
+      var td = document.createElement("td");
+      td.className = "bme-td-icon";
+      td.appendChild(icon);
+      tr.appendChild(td);
 
-            var td = document.createElement('td');
-            td.appendChild(text_div)
-            tr.appendChild(td)
+      var td = document.createElement("td");
+      td.appendChild(text_div);
+      tr.appendChild(td);
 
-            tbdy.appendChild(tr);
-        }
-        table.appendChild(tbdy);
-        wrapper.appendChild(table);
+      tbdy.appendChild(tr);
+    }
+    table.appendChild(tbdy);
+    wrapper.appendChild(table);
 
-        return wrapper;
-    },
+    return wrapper;
+  },
 
-    socketNotificationReceived: function (notification, payload) {
-        if (notification === 'DATA') {
-            this.temperature = payload.temp;
-            this.humidity = payload.humidity;
-            this.pressure = payload.press;
-            this.updateDom();
-        }
-    },
+  socketNotificationReceived: function (notification, payload) {
+    if (notification === "DATA") {
+      this.temperature = payload.temp;
+      this.humidity = payload.humidity;
+      this.pressure = payload.press;
+      this.updateDom();
+    }
+  }
 });
--- node_helper.js
+++ node_helper.js
@@ -1,4 +1,4 @@
-'use strict';
+"use strict";
 
 /* Magic Mirror
  * Module: MMM-BME280
@@ -7,36 +7,91 @@
  * MIT Licensed.
  */
 
-const NodeHelper = require('node_helper');
-const exec = require('child_process').exec;
+const NodeHelper = require("node_helper");
+const exec = require("child_process").exec;
+const https = require("https");
 
 module.exports = NodeHelper.create({
-	start: function () {
-		console.log('BME280 helper started ...');
-	},
+  start: function () {
+    console.log("BME280 helper started ...");
+  },
 
-	// Subclass socketNotificationReceived received.
-	socketNotificationReceived: function (notification, payload) {
-		const self = this;
-		if (notification === 'REQUEST') {
-			const self = this
-			this.config = payload
-			var deviceAddr = this.config.deviceAddress;
+  // Subclass socketNotificationReceived received.
+  socketNotificationReceived: function (notification, payload) {
+    const self = this;
+    if (notification === "REQUEST") {
+      const self = this;
+      this.config = payload;
+      var deviceAddr = this.config.deviceAddress;
+      var iotplotter_feed = this.config.iotplotter_feed;
+      var iotplotter_api_key = this.config.iotplotter_api_key;
 
-			// execute external DHT Script
-			exec(`python3 ./modules/MMM-BME280/bme280.py ${deviceAddr}`, (error, stdout) => {
-				if (error) {
-					console.error(`exec error: ${error}`);
-					return;
-				}
-				var arr = stdout.split(" ");
-				// Send data
-				self.sendSocketNotification('DATA', {
-					temp: arr[0],
-					humidity: arr[1],
-					press: arr[2],
-				});
-			});
-		}
-	}
+      // execute external DHT Script
+      exec(
+        `python3 ./modules/MMM-BME280/bme280.py ${deviceAddr}`,
+        (error, stdout) => {
+          if (error) {
+            console.error(`exec error: ${error}`);
+            return;
+          }
+          var arr = stdout.split(" ");
+          // Send data
+          self.sendSocketNotification("DATA", {
+            temp: arr[0],
+            humidity: arr[1],
+            press: arr[2]
+          });
+
+          // Send data to iotplotter
+
+          const data = JSON.stringify({
+            data: {
+              temperature: [
+                {
+                  value: arr[0]
+                }
+              ],
+              humidity: [
+                {
+                  value: arr[1]
+                }
+              ],
+              barometer: [
+                {
+                  value: arr[2]
+                }
+              ]
+            }
+          });
+
+          const options = {
+            hostname: "iotplotter.com",
+            port: 443,
+            path: `/api/v2/feed/${iotplotter_feed}`,
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json",
+              "Content-Length": data.length,
+              "api-key": iotplotter_api_key
+            }
+          };
+
+          const req = https.request(options, (res) => {
+            console.log(`statusCode: ${res.statusCode}`);
+
+            res.on("data", (d) => {
+              process.stdout.write(d);
+            });
+          });
+
+          req.on("error", (error) => {
+            console.error(error);
+          });
+
+          req.write(data);
+          req.end();
+        }
+      );
+    }
+  }
 });
